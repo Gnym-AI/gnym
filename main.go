c41287baa8cdbba0c0443eb0168099b83c3410d2
snoogans235 <johnny.newtron@gmail.com>
2026-08-20 19:35:43 -0400

    setup new testing container and enable mutation testing

--- main.go
+++ main.go
@@ -1,26 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"gnym/review"
-	"os"
-)
-
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: gnym review")
-		return
-	}
-
-	command := os.Args[1]
-	switch command {
-	case "review":
-		review.Run()
-
-	case "version":
-		fmt.Println("Gnym v0.1.0")
-
-	default:
-		fmt.Println("Unknown command:", command)
-	}
-}
+func main() {}
